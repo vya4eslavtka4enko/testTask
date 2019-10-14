12bc46a13e35141500031d6ac80f8367f7483e67
Montage <Montage@DJR-iMac-110-370.local>
2019-10-14 18:38:52 +0300

    work

--- js/app.js
+++ js/app.js
@@ -8,11 +8,25 @@ $(document).ready(function() {
        });
        return false;
     });
+    $("#buttonSend").click(function() {
+      $("html, body").animate({
+         scrollTop: $($(this).attr("href")).offset().top + "px"
+      }, {
+         duration: 1500,
+         easing: "swing"
+      });
+      return false;
+   });
  });
  const redDiploma = document.querySelector('.redDiploma__image-front');
-
+ const coverImage = document.querySelector('.coverImage')
  redDiploma.addEventListener('click',function(){
-   redDiploma.style.position = "absolute";
-   redDiploma.style.width = "100%";
-   redDiploma.style.height ="100vh";
+   if(coverImage.style.display == 'block'){
+      coverImage.style.display = 'none'
+   }else{
+      coverImage.style.display = 'block' ;
+      coverImage.style.position= 'fixid' ;
+      coverImage.style.left = '10%'
+   }
+   
  });
